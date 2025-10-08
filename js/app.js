08617cad0db445edb19a45c33ac38fdef040844b
Cedric <crubbertire@rogers.com>
2025-10-08 12:47:28 -0400

    Created and styled Pokedex Page

--- js/app.js
+++ js/app.js
@@ -4,6 +4,67 @@ let limit = 20;
 
 
 
+// --------------------------
+// Filter Containers
+// --------------------------
+
+let typeFilters = [];
+
+
+
+const filterElem = document.getElementById("type-filter");
+const typeContainer = document.getElementById("type-container")
+filterElem.addEventListener("click", () => {
+  typeContainer.classList.toggle("open");
+});
+
+const filterDropDown = document.querySelector(".filter-drop-down");
+filterDropDown.addEventListener("click", (event) => {
+  const targetFilter = event.target;
+  const filterID = event.target.id;
+  if (filterID === "any") {
+    if (!targetFilter.classList.contains("selected")) {
+      const allTypes = document.querySelectorAll(".type");
+      allTypes.forEach((type) => {
+        if (!typeFilters.includes(type.id)) {
+          if (type.id != "any") {
+            typeFilters.push(type.id);
+          }
+          type.classList.add("selected");
+        }
+      })
+    } else {
+      typeFilters = [];
+      const selecteds = document.querySelectorAll(".selected");
+      selecteds.forEach((item) => {
+        item.classList.remove("selected");
+      })
+    }
+  } else if (typeFilters.includes(filterID)) {
+    typeFilters = typeFilters.filter(item => item !== filterID);
+  } else {
+    typeFilters.push(filterID);
+  }
+  if (filterID != "any") {
+    targetFilter.classList.toggle("selected")
+  }
+  console.log(typeFilters);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const fetchData = async (params) => {
@@ -27,4 +88,4 @@ const fetchData = async (params) => {
 
 
 
-fetchData("pokemon/22");
+generatePokedex(fetchData("pokemon/129"));
